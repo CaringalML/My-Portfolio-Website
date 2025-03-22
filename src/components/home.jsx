861e832b432efa99fe69fc2b9c88d1aa512baa15
caringallawrence <caringallawrence5@gmail.com>
2025-03-23 00:19:11 +1300

    update diagram

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -91,7 +91,7 @@ const Portfolio = () => {
     {
       title: "CloudStruct-Enterprise-Serverless-Architecture-with-AWS-Fargate-Lambda-Aurora-and-CI-CD-Pipeline",
       description: "A highly available and scalable AWS infrastructure using Terraform that deploys a Backend Web API application with ECS Fargate for containerization, RDS Aurora MySQL for database, ALB for load balancing, and Route 53 for DNS management, all secured within a VPC with public and private subnets across multiple availability zones.",
-      image: "/img/ECS-Fargate.png",
+      image: "/img/AWS-ECS-Fargate.png",
       services: [
         "ECS with Fargate",
         "ECR",
@@ -102,7 +102,7 @@ const Portfolio = () => {
         "CloudWatch"
         
     ],
-      github: "https://github.com/CaringalML/AWS-Laravel-RESTful-API-Backend-Infrastructure",
+      github: "https://github.com/CaringalML/CloudStruct-Enterprise-Serverless-Architecture-with-AWS-Fargate-Lambda-Aurora-and-CI-CD-Pipeline.git",
       architecture: [
         "Route 53", 
         "ALB",
