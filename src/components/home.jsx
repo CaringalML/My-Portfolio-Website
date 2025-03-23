e6fa6222a3f953bd7754f46212983ca81016da44
caringallawrence <caringallawrence5@gmail.com>
2025-03-23 19:23:29 +1300

    ins

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -121,7 +121,7 @@ const Portfolio = () => {
     {
       title: "Artisan Tiling React + Vite Cloudfront",
       description: "A modern, responsive website for Artisan Tiling, a professional tiling company based in Hamilton, New Zealand. Built with React and Vite, deployed on AWS using Terraform for infrastructure management, with automated CI/CD through GitHub Actions.",
-      image: "/img/frontend-artisan-tiling.png",
+      image: "/img/frontend-artisan-tilings.png",
       services: [
         "Route53",
         "CloudFront",
@@ -144,7 +144,7 @@ const Portfolio = () => {
     {
       title: "Artisan Tiling Node-JS Lambda DynamoDB Serverless",
       description: "A serverless Node.js/Express RESTful API built for Artisan Tiling NZ, leveraging AWS API Gateway, Lambda with DynamoDB integration. This RESTful API handles Inventory Management System with comprehensive logging and monitoring capabilities, deployed using Terraform and Makefile for infrastructure management.",
-      image: "/img/api-gateway-lambda-dynamodbs.png",
+      image: "/img/api-gateway-lambda-dynamodb.png",
       services: [
         "Route 53",
         "API Gateway",
